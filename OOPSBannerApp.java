e54176f9b3d1bc3dd4419d1cda131c452a4e36aa
rohanar17 <rohann17012005@gmail.com>
2026-02-21 14:56:31 +0530

    UC5: Inline array initialization with String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,51 +2,50 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] lines = new String[7];
+        String[] lines = {
+                String.join(" ",
+                        "  *****  ",
+                        "  *****  ",
+                        " ******  ",
+                        "  *****  "),
 
-        lines[0] = String.join(" ",
-                "  *****  ",
-                "  *****  ",
-                " ******  ",
-                "  *****  ");
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " *     * ",
+                        " *     * "),
 
-        lines[1] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *     * ",
-                " *     * ");
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " *     * ",
+                        " *       "),
 
-        lines[2] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *     * ",
-                " *       ");
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " ******  ",
+                        "  *****  "),
 
-        lines[3] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " ******  ",
-                "  *****  ");
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " *       ",
+                        "       * "),
 
-        lines[4] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *       ",
-                "       * ");
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " *       ",
+                        " *     * "),
 
-        lines[5] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *       ",
-                " *     * ");
+                String.join(" ",
+                        "  *****  ",
+                        "  *****  ",
+                        " *       ",
+                        "  *****  ")
+        };
 
-        lines[6] = String.join(" ",
-                "  *****  ",
-                "  *****  ",
-                " *       ",
-                "  *****  ");
-
-        // loop printing (this is the UC4 upgrade)
         for (String line : lines) {
             System.out.println(line);
         }
